cb4948e312f7c43fb868f8fe2c6129088763cc20
famzyjr <famuyiwaahmed132@gmail.com>
2026-07-08 00:12:38 +0100

    feat:added active class  the navbar

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('name')
+const homes = document.querySelectorAll('.Home');
+
+
+homes.forEach(items =>{
+items.addEventListener('click',function() {
+homes.forEach(links => links.classList.remove('active'));
+items.classList.add('active');
+});
+
+})
+
+
+
+// navItems.forEach(item => {
+//             item.addEventListener('click',
+//                 function () {
+//                     navItems.forEach(navItem => navItem
+//                         .classList.remove('active'));
+//                     this.classList.add('active');
+//                 });
+//         });
+    
